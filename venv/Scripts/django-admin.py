616d7cfcc34562427bbbe28d6484bdeb9ef53bb8
daisysj <36334278+daisysj@users.noreply.github.com>
2018-10-24 19:05:34 -0400

    update github

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\study\BU Fall 2018\CS411 Software ENG\Project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
